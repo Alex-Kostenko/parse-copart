f772ecba62d78d8c7898d1ea28838bc088bc7fba
angieene <angela.nerukh@gmail.com>
2023-03-28 16:52:41 +0300

    add crone function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarsModule } from './cars/cars.module';
 import { configService } from './config/config.service';
@@ -11,6 +12,7 @@ import { ParserModule } from './parser/parser.module';
     HttpModule,
     CarsModule,
     ParserModule,
+    ScheduleModule.forRoot(),
   ],
 })
 export class AppModule {}
--- src/parser/parser.controller.ts
+++ src/parser/parser.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { IPositiveRequest } from 'src/core/types/main';
 import { ParserService } from './parser.service';
 
 @Controller('parser')
 export class ParserController {
   constructor(private readonly parserService: ParserService) {}
-
+  // @Cron(CronExpression.EVERY_DAY_AT_9AM)
   @Get('csvs')
   parseCSV(): Promise<IPositiveRequest> {
     return this.parserService.parseCSVs();
   }
 
+  // @Cron(CronExpression.EVERY_DAY_AT_10AM)
   @Get('cars')
   parseCars(): Promise<IPositiveRequest> {
     return this.parserService.parseCars();
